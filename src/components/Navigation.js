5c16ed374b61cf6dd5b4490710251aff0cc5e656
Namukk <skadnr625@gmail.com>
2021-02-05 23:52:53 +0900

    Fixed Error

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,1 +1,6 @@
+import React from "react";
+const Navigation = () => {
+  <span>남욱's page</span>;
+};
 
+export default Navigation;
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { HashRouter as Router, Route } from "react-router-dom";
+import Navigation from "./Navigation";
 
 const AppRouter = () => {
   return (
     <Router>
       <Route exact path="/">
         Home
+        <Navigation />
       </Route>
       <Route exact path="/profile">
         Profile
