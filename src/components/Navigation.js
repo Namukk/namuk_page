5b30549f812082be2a747fa2cada3be76eba7bce
Namukk <skadnr625@gmail.com>
2021-02-09 22:03:38 +0900

    navBar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,21 @@
 import React from "react";
+import styled from "styled-components";
+
+const List = styled.ul`
+  list-style: none;
+`;
+
 const Navigation = () => {
-  <span>남욱's page</span>;
+  return (
+    <div>
+      <span>남욱's page</span>
+      <List>
+        <li>About me</li>
+        <li>Projects</li>
+        <li>W.I.L</li>
+      </List>
+    </div>
+  );
 };
 
 export default Navigation;
